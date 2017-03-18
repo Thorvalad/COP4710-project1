bf2ffb21180fe28f66c638c2de14d1094d5d6889
mpd18 <mpd18@knights.ucf.edu>
2017-03-18 17:13:56 -0400

    Update eventsPage.php

--- eventsPage.php
+++ eventsPage.php
@@ -1,2 +1,33 @@
 <?php
-echo "this page currently is empty, haven't started it yet.";
+session_start();
+if(!isset($_SESSION['userClass']))
+{
+	header("location: frontPage.php");
+	exit();
+}
+else
+{
+	$userClass=$_SESSION['userClass'];
+}
+?>
+
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="topnav">	
+  <a class="active" href="eventspage.php">Home</a>
+  <a href="createRSO.php">Create a new RSO</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+<div style="padding-left:16px">
+  <h2>Top Navigation Example</h2>
+  <p>Some content..</p>
+</div>
+
+</body>
+</html>
